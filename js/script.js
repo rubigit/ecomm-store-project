00b60865c97553eaa8b3d84dc67cea35f2ee68df
rubigit <rcortes1708.rcj@gmail.com>
2021-04-25 18:36:24 -0400

    Set structure for the first product

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const shortProductDescription = `Here is a shot of this product that might entice a user to click and add it to their cart.`
+const products = [
+    {
+        name: `Vavi`,
+        type: `Table & Desk Lamp`,
+        price: { regPrice: 50, salePrice: 39 },
+        shortdescription: shortProductDescription,
+        rate: `4.5`,
+        picture: { img1: `desk-lamp-01-480w.jpg`, img2: `desk-lamp-01-960w.jpg`img3: `desk-lamp-01-1920w.jpg` }
+    }
+]
