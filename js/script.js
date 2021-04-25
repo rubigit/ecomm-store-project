c1e82134fd6690c89949192b71c1f9eff8daa448
rubigit <rcortes1708.rcj@gmail.com>
2021-04-25 19:40:52 -0400

    Display first product using js script

--- js/script.js
+++ js/script.js
@@ -2,10 +2,44 @@ const shortProductDescription = `Here is a shot of this product that might entic
 const products = [
     {
         name: `Vavi`,
-        type: `Table & Desk Lamp`,
+        category: `Table & Desk Lamp`,
+        color: `Black`,
+        bulbType: `Incandescent`,
         price: { regPrice: 66, salePrice: 55 },
         shortdescription: shortProductDescription,
         rate: `4.5`,
         picture: `desk-lamp-01-480w.jpg`,
+        picDescription: `Two table lamps in a bedroom`
     }
 ]
+
+const productTable = document.querySelector(`#results`)
+products.forEach((product) => {
+    alert(`asdasd`)
+    const displayProduct = document.createElement(`article`)
+    displayProduct.classList.add(`product`)
+    displayProduct.innerHTML = `
+    <header>
+          <img src="img/${product.picture}" alt="${product.picDescription}" height="320" />
+          <div class=prod-name-price>
+            <h3>${product.name} ${product.category}</h3>
+            <data value="${product.price.salePrice} "><del>$${product.price.regPrice}.00</del><ins>$${product.price.salePrice}.00</ins></data>
+          </div>
+          <div class="prod-description-rate">
+            <p>${product.shortdescription}</p>
+            <dl>
+              <dt>Rating</dt>
+              <dd><span class="material-icons">star</span><span class="material-icons">star</span><span
+                  class="material-icons">star</span><span class="material-icons">star</span><span
+                  class="material-icons">star_half</span><span>${product.rate}</span></dd>
+            </dl>
+          </div>
+          <a href="./product.html">see more</a>
+        </header>
+        <footer>
+          <button class="product-like"><span class="produc-icon-text">.</span></button>
+          <button class="product-cart"><span class="produc-icon-text">.</span></button>
+        </footer>
+    `
+    productTable.appendChild(displayProduct)
+})
